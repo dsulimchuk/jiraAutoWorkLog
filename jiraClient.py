478ca3cbabe363971688896dc4a978b2944d65f4
dmitrii.sulimchuk <dmitrii.sulimchuk@nexign-systems.com>
2019-01-25 14:45:34 +0300

    cleanup

--- jiraClient.py
+++ jiraClient.py
@@ -1,13 +1,13 @@
 from jira import JIRA
 
 
-class JiraTimeAggregator:
-    seconds_in_day = 28800
+class JiraClient:
+    seconds_in_hour = 3600
 
     def __init__(self, url, login, password) -> None:
         self.jira = JIRA(url, auth=(login, password))
 
-    def query(self, jql_str):
+    def queryWorklog(self, jql_str):
         issues = self.jira.search_issues(
             jql_str=jql_str,
             json_result=True,
@@ -18,19 +18,33 @@ class JiraTimeAggregator:
         timespent = 0
         originalEstimate = 0
         for row in issues['issues']:
-            print(row['key'], "  ", row['fields'])
+            # print(row['key'], "  ", row['fields'])
             if row['fields']['timespent'] is not None:
                 timespent += row['fields']['timespent']
             if row['fields']['timeoriginalestimate'] is not None:
                 originalEstimate += row['fields']['timeoriginalestimate']
 
-        print(issues)
-        print(timespent / self.seconds_in_day)
-        print(originalEstimate / self.seconds_in_day)
-        return {'timespent': timespent / self.seconds_in_day,
-                'originalEstimate': originalEstimate / self.seconds_in_day}
+        return {'timespent': timespent / self.seconds_in_hour,
+                'originalEstimate': originalEstimate / self.seconds_in_hour}
+
+    def queryIssues(self, jql_str, fetch_size=10000):
+        issues = self.jira.search_issues(
+            jql_str=jql_str,
+            json_result=True,
+            maxResults=fetch_size,
+            fields=None
+        )
+
+        return issues['issues']
+
+    def add_worklog(self, issue, hours):
+        self.jira.add_worklog(
+            issue,
+            timeSpentSeconds=hours * self.seconds_in_hour,
+            comment="auto log"
+        )
 
 
 if __name__ == '__main__':
-    JiraTimeAggregator(None, None, None).query(
+    JiraClient(None, None, None).queryWorklog(
         'parent in (CCM-10722, CCM-10723, CCM-10776, CCM-10738) AND issueFunction in aggregateExpression("Total Estimate for All Issues", "originalEstimate.sum()", "Remaining work", "remainingEstimate.sum()", "Time Spent", "timeSpent.sum()") ORDER BY updated DESC')
--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 import csv
 import getpass
-from datetime import datetime
+from datetime import date
 
 import keyring
 
-from jiraTimeAggregator import JiraTimeAggregator
+from jiraClient import JiraClient
 
 JIRA_URL = 'https://jira.billing.ru'
 JIRA_USER = 'dmitrii.sulimchuk'
+PROJECT = "CCM-CCM"
 
 
 def main():
     jira_client = init_jira()
+    now = date.today()
+    working_days_in_current_month = workingdays(now)
 
-    issues = jira_client.query(
-        "assignee = Dmitrii.Sulimchuk  and status not in (Closed, Backlog, Open) and project = CCM-CCM ORDER BY timespent")
-    print(issues)
-    now = datetime.now()
-    time = str(now)
-    current_month = now.month
-    current_year = now.year
-    print(time)
-    holidays = getAllHolidays(current_month, current_year)
-
-    data = [time, issues['originalEstimate'], issues['timespent']]
-
-    print("Try to append: ", data)
-    # gservice.append(data)
+    for day in working_days_in_current_month:
+        spent = findAlreadyLogged(jira_client, day)
+        if spent < 8:
+            add_work_log(jira_client, day, 8 - spent)
+        else:
+            print(f"at {day} all ok!")
 
 
 def getAllHolidays(current_month, current_year):
@@ -36,19 +31,45 @@ def getAllHolidays(current_month, current_year):
             if row[0] == str(current_year):
                 row[current_month].split(',')
                 all_holidays_in_month = set(map(lambda x: int(x), row[current_month].split(',')))
-                print(f'All holidays in {current_year}.{current_month}: {all_holidays_in_month}')
+                # print(f'All holidays in {current_year}.{current_month}: {all_holidays_in_month}')
                 return all_holidays_in_month
+
+
+def workingdays(now):
+    holidays = getAllHolidays(now.month, now.year)
+    result = []
+    for i in range(1, date.today().day):
+        if i not in holidays:
+            result.append(date.today().replace(day=i))
+    return result
 
 
 def init_jira():
     jira_password = keyring.get_password(JIRA_URL, JIRA_USER)
     if jira_password is None:
         jira_password = getpass.getpass()
-        jira_client = JiraTimeAggregator(JIRA_URL, JIRA_USER, jira_password)
+        jira_time_aggregator = JiraClient(JIRA_URL, JIRA_USER, jira_password)
         keyring.set_password(JIRA_URL, JIRA_USER, jira_password)
-        return jira_client
-    jira_client = JiraTimeAggregator(JIRA_URL, JIRA_USER, jira_password)
-    return jira_client
+        return jira_time_aggregator
+    jira_time_aggregator = JiraClient(JIRA_URL, JIRA_USER, jira_password)
+    return jira_time_aggregator
+
+
+def findAlreadyLogged(jira_client, day):
+    time = jira_client.queryWorklog(
+        f'assignee = currentUser() AND worklogAuthor =currentUser() AND worklogDate = "{day}"',
+    )
+    return time["timespent"]
+
+
+def add_work_log(jira_client, day, hours):
+    issue = jira_client.queryIssues(
+        f"assignee = currentUser()  and status not in (Closed, Backlog, Open, Resolved) and project = {PROJECT} ORDER BY timespent DESC",
+        fetch_size=1
+    )[0]
+    print(f"i want to log {hours} hour(s) at {day} to {issue['key']}")
+    jira_client.add_worklog(issue, hours)
+
 
 
 if __name__ == '__main__':
